7ed8dee78ba52e965286dc0964ff83976139d066
LeoLuisBispo <l.luisbispo@outlook.com>
2022-09-15 16:01:54 +0200

    outro push

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import NotificationButton from "./components/notificationButton";
 
 function App() {
-
   return (
     <div className="App">
       <h1>niente</h1>
+      <NotificationButton />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
